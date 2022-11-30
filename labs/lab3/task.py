05426eec670448ad5fa2bdcc017d0564018f3b1e
timo364 <70896036+timo364@users.noreply.github.com>
2022-12-01 00:07:11 +0200

    working from gdrive dir is like a hell...

--- labs/lab3/task.py
+++ labs/lab3/task.py
@@ -1,12 +1,19 @@
 import cv2 as cv
-
 from pathlib import Path
 
 
-# DIR = Path().absolute().joinpath(__file__).parent
 DIR = Path(__file__).parent
 
-
 # https://github.com/opencv/opencv/blob/master/data/haarcascades/haarcascade_frontalface_default.xml
-CASCADE = cv.CascadeClassifier(f'{DIR}/haarcascade_frontalface_default.xml')
+# CASCADE = cv.CascadeClassifier(f'{DIR}\haarcascade_frontalface_default.xml')
 
+
+def detect_face():
+    imgs_dir = DIR.joinpath('photos')
+    for img in imgs_dir.iterdir():
+        print(img)
+        current = cv.imread(str(img))
+        cv.imshow('.', current)
+        break
+
+detect_face()
