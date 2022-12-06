54f14f08749b2f262751e8c5694e0937e45acfff
timo364 <70896036+timo364@users.noreply.github.com>
2022-12-06 23:12:11 +0200

    lab4: done.

---
+++ labs/lab4/task.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from asyncio import run
+from time import perf_counter
+
+from requests import get
+from aiohttp import ClientSession
+from aiofiles import open as async_open
+
+
+DIR = Path(__file__).parent
+
+
+def download_files_synchronously():
+    out_dir = DIR.joinpath('out_sync')
+    out_dir.mkdir(parents=True, exist_ok=True)
+    [f.unlink() for f in out_dir.iterdir()]
+
+    with open(f'{DIR}\links.txt') as links_file:
+        links = links_file.readlines()
+
+    for link in links:
+        link = link.replace('\n', '')
+        filename = link.split('/')[-1]
+        response = get(link)
+
+        with open(f'{out_dir}\{filename}', 'wb') as out_file:
+            out_file.write(response.content)
+
+
+async def download_files_asynchronously():
+    out_dir = DIR.joinpath('out_async')
+    out_dir.mkdir(parents=True, exist_ok=True)
+    [f.unlink() for f in out_dir.iterdir()]
+
+    async with async_open(f'{DIR}\links.txt') as links_file:
+        async for link in links_file:
+            link = link.replace('\n', '')
+            filename = link.split('/')[-1]
+
+            async with ClientSession() as session:
+                async with session.get(link) as resp:
+                    content = await resp.read()
+
+            async with async_open(f'{out_dir}\{filename}', 'wb') as out_file:
+                await out_file.write(content)
+
+
+if __name__ == '__main__':
+    sync_time = perf_counter()
+    download_files_synchronously()
+    print(f'Synchronous execution time: {perf_counter() - sync_time:0.2f} msecs.')
+
+    async_time = perf_counter()
+    run(download_files_asynchronously())
+    print(f'Asynchronous execution time: {perf_counter() - async_time:0.2f} msecs.')
